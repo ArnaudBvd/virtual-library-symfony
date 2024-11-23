79cd1cd6b61d142f5369e1edc3aecacf35a3be26
ArnaudBvd <bravard.arnaud@gmail.com>
2024-11-23 18:07:08 +0100

    refactor: import du Trait IdNameTrait dans l'entité Status

--- src/Entity/Status.php
+++ src/Entity/Status.php
@@ -2,11 +2,12 @@
 
 namespace App\Entity;
 
+use App\Entity\Trait\IdNameTrait;
 use App\Repository\StatusRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: StatusRepository::class)]
 class Status
 {
-    use Trait\IdNameTrait;
+    use IdNameTrait;
 }
