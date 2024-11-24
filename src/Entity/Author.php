358b158a56db9418a32f7bceb6b1b6d56b8714de
ArnaudBvd <bravard.arnaud@gmail.com>
2024-11-24 16:21:01 +0100

    refactor: utilisation du trait IdNameTrait dans l'entité Author

--- src/Entity/Author.php
+++ src/Entity/Author.php
@@ -2,19 +2,12 @@
 
 namespace App\Entity;
 
+use App\Entity\Trait\IdNameTrait;
 use App\Repository\AuthorRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: AuthorRepository::class)]
 class Author
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+    use IdNameTrait;
 }
